827bcc2b42faf933f7a3afa214355b4afb69f3e8
Trey Deitch <me@tdeitch.com>
2011-08-12 20:06:07 -0500

    Remove dependence on external server to process RSS feed

--- logoreplace.js
+++ logoreplace.js
@@ -1,18 +1,22 @@
 var req = new XMLHttpRequest();
 req.open(
-    "GET",
-    "http://tdeitch.com/art",
+    'GET',
+    'http://feeds.feedburner.com/ArtistADay-ArtSubmissionsAccepted',
     true);
 req.onload = replaceLogo;
 req.send(null);
 
 function replaceLogo() {
-    var originalLogo = document.getElementById("lga");
-//    var form = document.getElementsByName("f")[0];
-    var myDiv = document.createElement("div");    
-    myDiv.id = 'lga';
-    myDiv.innerHTML = req.responseText;
-//    form.parentNode.insertBefore(myDiv, form)
-//    originalLogo.parentNode.removeChild(originalLogo);
-    originalLogo.parentNode.replaceChild(myDiv, originalLogo);
+    // use a regular expression to find the image URL
+    var imgPattern = new RegExp('img src="(http://www.aadstatic.com/uploads/[A-Za-z0-9\_]+\.jpg)"');
+    var imgURL = imgPattern.exec(req.responseText)[1];
+    // create a new logo div
+    var newLogo = document.createElement('div');    
+    newLogo.id = 'lga';
+    // insert the logo into the new div
+    newLogo.innerHTML = '<a href="http://www.artistaday.com"><img id="art" src="'+imgURL+'" height="231px"></a>';
+    // find the old logo and replace it with the new one
+    var oldLogo = document.getElementById('lga');
+    oldLogo.parentNode.replaceChild(newLogo, oldLogo);
+    // move the search form out of the art's way
 }
